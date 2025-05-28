65d194e8b41ffc7517dd078c0a4ebed65bd8060e
Muhammad Sahril <muhammadsahrilid@gmail.com>
2025-05-28 09:37:07 +0700

    Pertemuan-14

---
+++ README.txt
@@ -0,0 +1,1 @@
+Pertemuan 14 - tugas pindahkan Script di pertemuan 10 11 12 ke vscode
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PegawaiDBController;
 use App\Http\Controllers\DosenController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,12 +17,9 @@ Route::view('tugas-linktree','tugas-linktree');
 Route::view('ETS','ETS');
 
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
-Route::get('/formulir', [PegawaiController::class, 'formulir']);
-Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+// // Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+// Route::get('/formulir', [PegawaiController::class, 'formulir']);
+// Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+// Route::get('/dosen', [DosenController::class, 'index']);
 
-
-
-
-
-Route::get('/dosen', [DosenController::class, 'index']);
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
